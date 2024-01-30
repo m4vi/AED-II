aa1ee6677ee2dec5b51c2875f2cf7f954eb1c613
m4vi <vitlia40@gmail.com>
2024-01-29 22:08:30 -0400

    mm

--- Unidade 2/exer3.c
+++ Unidade 2/exer3.c
@@ -23,39 +23,37 @@ void cadastro(funcionario *Est, departamento d[], int *pos_dep)
     printf("Digite o nome: ");
     fflush(stdin);
     gets(Est->nome);
-    printf("Digite o codigo ");
 
+    printf("Digite o codigo ");
     scanf("%d", &Est->codigo);
 
     printf("Digite departamento: ");
     fflush(stdin);
-    // gets(Est->dep.nome);
+    gets(Est->dep->nome);
 
+    int flag = 0;
     for (int i = 0; i < TAM; i++)
     {
-        if (strcmp(d[i].nome, gets((Est->dep->nome)) == 0)) // se exister um dep igual só atribui
+        if (strcmp(d[i].nome, Est->dep->nome) == 0) // se exister um dep igual só atribui
         {
-            *(Est->dep) = d[i];
+            Est->dep = &d[i];
+            flag = 1;
             break;
         }
-        else // se nao cria o dep
-        {
-            if (*pos_dep < 5)
-            {
-                printf("digite nome do dep: ");
-                fflush(stdin);
-                gets(d[*pos_dep].nome);
+    }
+    if ((*pos_dep < 5) && (flag = 1))
+    {
+        printf("digite nome do dep: ");
+        fflush(stdin);
+        gets(d[*pos_dep].nome);
 
-                printf("digite codigo do dep: ");
-                scanf("%d", &d[*pos_dep].codigo);
-                (*pos_dep)++;
-            }
-            else
-            {
-                printf("departamentos maximo");
-            }
-            break;
-        }
+        printf("digite codigo do dep: ");
+        scanf("%d", &d[*pos_dep].codigo);
+        (*pos_dep)++;
+    }
+    else
+    {
+        printf("departamentos maximo");
     }
 }
 
@@ -63,7 +61,7 @@ void listar(funcionario f[], int *pos)
 {
     for (int i = 0; i < *pos; i++)
     {
-        printf("\nNome: %s, codigo: %d, departamento: %s, dep cod: %d", f[i].nome, f[i].codigo, f[i]->dep.nome, f[i]->dep.codigo);
+        printf("\nNome: %s, codigo: %d, departamento: %s, dep cod: %d", f[i].nome, f[i].codigo, f[i].dep->nome, f[i].dep->codigo);
     }
 }
 void listar_D(departamento f[], int *pos)
@@ -81,14 +79,14 @@ void atualizar(funcionario f[], int *pos, departamento d[], int *pos_dep)
     printf("1 - nome\n");
     printf("2 - codigo nome \n");
     printf("3 - departamento nome\n");
-    printf("4 - departamento coigo\n");
+    printf("4 - departamento codigo\n");
     printf("5 - departamento de um funcionario\n");
     printf("O que deseja atualizar: ");
     scanf("%d", &opc);
 
     switch (opc)
     {
-    int pos_aux, pos_aux2;
+        int pos_aux, pos_aux2;
     case 1:
         listar(f, pos);
         printf("digite a posicao ");
@@ -97,9 +95,9 @@ void atualizar(funcionario f[], int *pos, departamento d[], int *pos_dep)
         fflush(stdin);
         gets(f[pos_aux].nome);
         break;
-    case 2: 
+    case 2:
         listar(f, pos);
-        
+
         printf("digite a posicao ");
         scanf("%d", &pos_aux);
         printf("Digite o codigo: ");
@@ -122,14 +120,14 @@ void atualizar(funcionario f[], int *pos, departamento d[], int *pos_dep)
         fflush(stdin);
         scanf("%d", &d[pos_aux].codigo);
         break;
-    case 5: 
+    case 5:
         listar(f, pos);
         printf("digite a posicao do funcionario"); // POSICAO DO VETOR F
         scanf("%d", &pos_aux);
         listar_D(d, pos_dep);
         printf("Digite a posicao do departamento que vc quer mandar o funcionario: "); // POSICAO VETOR D
         scanf("%d", &pos_aux2);
-        (f[pos_aux].dep) = d[pos_aux2];
+        (f[pos_aux].dep) = &(d[pos_aux2]);
         break;
     default:
         break;
@@ -139,20 +137,19 @@ int main(void)
 {
     int pos_fun = 0, pos_dep = 0;
     int opcao;
+    departamento d[TAM];
+    funcionario f[TAM];
     do
     {
-        departamento d[TAM];
-        funcionario f[TAM];
 
         printf("\n-1 - sair\n");
-        printf("1 - cadastros\n");
-        printf("2 - Custo do estacionamento pelas horas estacionadas (tarifa R$10,00 p/ hr)\n");
-        printf("3 - Registrar a saida dos veiculos\n");
-        printf("4 - Listagem de veiculos\n");
-        printf("Digite uma opcao: ");
+        printf(" 1 - cadastros\n");
+        printf(" 2 - Atualizar\n");
+        printf(" 3 - listar \n");
+        printf(" Digite uma opcao: ");
         scanf("%d", &opcao);
 
-        int pos_aux;
+        // int pos_aux;
         switch (opcao)
         {
         case 1:
@@ -163,60 +160,20 @@ int main(void)
             }
             else
             {
-                printf("O estacionamento esta cheio\n");
+                printf("esta cheio\n");
             }
             break;
         case 2:
             atualizar(f, &pos_fun, d, &pos_dep);
             break;
-        /*case 2:
-            if (pos != 0) // se o inventario tem algum item
-            {
-                printf("Qual posicao gostaria de calcular o custo do estacionamento? ");
-                scanf("%d", &pos_aux);
-                if (pos_aux < pos) // quer dizer q existe esse produto catalogado
-                {
-                    calculo_custo(&e[pos_aux]);
-                }
-                else
-                {
-                    printf("indice invalido\n");
-                }
-            }
-            else
-            {
-                printf("O estacionamento esta vazio\n");
-            }
-            break;
         case 3:
-            if (pos != 0) // se o est tem algum item
-            {
-                printf("Qual carro ira sair? ");
-                scanf("%d", &pos_aux);
-                if (pos_aux < pos) // quer dizer q existe esse produto catalogado
-                {
-                    saida_carro(e, &pos, &pos_aux);
-                }
-                else
-                {
-                    printf("indice invalido\n");
-                }
-            }
-            else
-            {
-                printf("O estacionamento esta vazio\n");
-            }
-
-            break;
-        */
-        case 4:
             if (pos_fun != 0)
             {
                 listar(f, &pos_fun);
             }
             else
             {
-                printf("O estacionamento esta vazio\n");
+                printf("esta vazio\n");
             }
             break;
         case -1:
